24d8b6f77a809ee3220442e65713163ce6bc3074
Jatin Yadu <jatinkyadu@gmail.com>
2026-06-03 22:07:53 +0530

    model: add staff schema

---
+++ src/models/staff.model.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { auditSchemaFields, baseSchemaOptions } from "./baseSchema";
+
+interface IStaff extends mongoose.Document {
+  username: string;
+  password: string;
+  location: mongoose.Types.ObjectId;
+  organization: mongoose.Types.ObjectId;
+  isActive: boolean;
+}
+
+const staffSchema = new mongoose.Schema<IStaff>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Location",
+      required: true,
+    },
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  baseSchemaOptions,
+);
+
+staffSchema.add(auditSchemaFields);
+
+const Staff = mongoose.model<IStaff>("Staff", staffSchema);
+
+export default Staff;
